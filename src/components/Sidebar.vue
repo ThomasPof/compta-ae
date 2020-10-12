79dc7209baa84a83ce50ecefbe79cf5254790dce
Thomas <thomaspopoff@gmail.com>
2020-10-12 11:27:53 +0200

    Add open-iconic (test)

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <nav class="nav flex-column nav-pills">
-      <router-link class="nav-link font-weight-bold" to="/dashboard">
+      <router-link class="nav-link font-weight-bold d-flex align-items-center mb-3" to="/dashboard">
         <!-- <img src="/images/balance-scale-solid.svg" alt="" height="18px" class="mr-2"> -->
+        <img src="/images/open-iconic/svg/dashboard.svg" height="18px" class="mr-2">
         Bilan annuel
       </router-link>
-      <router-link class="nav-link font-weight-bold" to="/factures">
+      <router-link class="nav-link font-weight-bold d-flex align-items-center mb-3" to="/factures">
         <!-- <img src="/images/receipt-solid.svg" alt="" height="18px" class="mr-2"> -->
+        <img src="/images/open-iconic/svg/dollar.svg" height="18px" class="mr-2">
         Factures
+      </router-link>
+      <router-link class="nav-link font-weight-bold d-flex align-items-center mb-3" to="/options">
+        <!-- <img src="/images/receipt-solid.svg" alt="" height="18px" class="mr-2"> -->
+        <img src="/images/open-iconic/svg/cog.svg" height="18px" class="mr-2">
+        Mon compte
+      </router-link>
+      <router-link class="nav-link font-weight-bold d-flex align-items-center mb-3" to="/options">
+        <!-- <img src="/images/receipt-solid.svg" alt="" height="18px" class="mr-2"> -->
+        <img src="/images/open-iconic/svg/account-logout.svg" height="18px" class="mr-2">
+        Se déconnecter
       </router-link>
     </nav>
   </div>
