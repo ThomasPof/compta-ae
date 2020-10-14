cc1d1cc25481c2f62f8921d99d6075f2de5b41b7
Thomas <thomaspopoff@gmail.com>
2020-10-14 14:33:32 +0200

    better invoices view

--- src/components/Factures.vue
+++ src/components/Factures.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
     <div class="bg-white rounded-lg p-4 shadow-sm">
-      <table class="table">
+      <p>Factures créés en {{ $aeoptions.year }}</p>
+      <table class="table table-striped">
         <thead>
           <tr>
-            <th scope="col">Référence</th>
+            <th class="text-center" scope="col">#</th>
+            <th class="text-center" scope="col">Statut</th>
+            <th scope="col">Entreprise</th>
             <!-- <th scope="col">Statut</th> -->
             <th scope="col">Date</th>
             <!-- <th scope="col">HT</th> -->
             <!-- <th scope="col">TVA</th> -->
             <th scope="col">Montant</th>
-            <th scope="col">Statut</th>
           </tr>
         </thead>
         <tbody v-if="$aeoptions.year">
           <tr v-for="(facture, index) in invoices" :key='index'>
-            <td>
-              <p class="mb-0 font-weight-bold">{{ facture.recipient.company }}</p>
+            <td class="text-center">
               <p class="mb-0 small">
                 <a :href="facture.url" target="_blank" class="text-muted">
                   {{ facture.uid }}
                 </a>
               </p>
+            </td>
+            <td class="text-center">
+              <p class="mb-0">
+                <span class="badge badge-pill badge-secondary" v-if="facture.paidAt">Réglée</span>
+                <span class="badge badge-pill badge-primary texte-white" v-else>Attente paiement</span>
+              </p>
+            </td>
+            <td>
+              <p class="mb-0">{{ facture.recipient.company }}</p>
             </td>
             <!-- <td></td> -->
             <td>{{ moment(facture.createdAt * 1000).locale('FR').format('ll') }}</td>
@@ -29,16 +39,8 @@
             <!-- <td>{{ facture.pricePretax }}</td> -->
             <!-- <td>{{ facture.taxAmount }}</td> -->
             <td class="text-right">
-              <p class="mb-0 text-primary font-weight-bold">{{ facture.pricePretax }}€</p>
-              <div v-if="facture.taxAmount">
-                <p class="text-muted mb-1 pb-1 small border-bottom" v-if="facture.taxAmount">+{{ facture.taxAmount }}€ TVA</p>
-                <p class="mb-0 small">{{ facture.price }}€ TTC</p>
-              </div>
-            </td>
-            <td>
-              <p class="mb-0">
-                <span class="badge badge-secondary" v-if="facture.paidAt">Réglée</span>
-                <span class="badge badge-primary texte-white" v-else>Attente paiement</span>
+              <p class="mb-0 text-primary font-weight-bold">{{ facture.pricePretax }}€
+                <span class="text-muted small" v-if="facture.taxAmount"> (+{{ facture.taxAmount }}€ TVA)</span>
               </p>
             </td>
           </tr>
@@ -70,6 +72,7 @@ export default {
           this.invoices.push(invoice)
 
         }
+        this.invoices.reverse()
         // console.log(invoice)
       }
     }
